b5f4f3972c3a3ea23d77f31fec4af638ecca9c66
Diego Martinoia <doctor_omega504@hotmail.com>
2013-03-27 16:25:10 +0100

    a

--- src/main/DataStruct.java
+++ src/main/DataStruct.java
@@ -1,19 +1,23 @@
 package main;
 
+import java.util.Date;
+
 public class DataStruct {
 	public float x;
 	public float y;
 	public float z;
 	public String id;
+	public Date timestamp;
 	
 	public DataStruct(float x, float y, float z, String id) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
 		this.id = id;
+		this.timestamp = new Date();
 	}
 	
 	public String toString() {
-		return id+" -> X: "+this.x+" ; "+"Y: "+this.y+" ; "+"Z: "+this.z+" ;";
+		return id+" -> X: "+this.x+" ; "+"Y: "+this.y+" ; "+"Z: "+this.z+" ; Received at "+this.timestamp.toString();
 	}
 }
