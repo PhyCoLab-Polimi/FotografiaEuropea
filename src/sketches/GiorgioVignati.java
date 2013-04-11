4c52852fdb00da9b697fb5e144ac3f412407ae5c
gvignati <gvignati@gmail.com>
2013-04-11 13:43:44 +0200

    comments

--- src/sketches/GiorgioVignati.java
+++ src/sketches/GiorgioVignati.java
@@ -12,12 +12,17 @@ public class GiorgioVignati extends EmptySketch {
 	}
 	
 	public void draw() {
+		//USA parent. per i metodi di finestra (color, font etc)
+		//usa PApplet. per le costanti (center, hsb, etc)
 		  parent.colorMode(PApplet.HSB, 360, 100, 100);
 		  parent.rectMode(PApplet.CENTER); 
 		  parent.noStroke();
 		  parent.background(parent.mouseY/2, 100, 100);
 
 		  parent.fill(360-parent.mouseY/2, 100, 100);
+		  
+		  //Non fare mai riferimenti assoluti alle dimensioni, ma sempre proporzionali
+		  //cosi se le dimensioni cambiano non devi stare a rifare tutto
 		  parent.rect(parent.width/2, parent.height/2, parent.mouseX+1, parent.mouseX+1);
 	}
 
