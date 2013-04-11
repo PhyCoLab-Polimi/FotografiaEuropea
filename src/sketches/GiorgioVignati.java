4467872569e6789e2591b019a714be23451573fa
gvignati <gvignati@gmail.com>
2013-04-11 13:42:23 +0200

    Fixed Giorgios work by Diego

--- src/sketches/GiorgioVignati.java
+++ src/sketches/GiorgioVignati.java
@@ -1,22 +1,24 @@
 package sketches;
 
+import processing.core.PApplet;
 import main.References;
 
 
 public class GiorgioVignati extends EmptySketch {
 	public void setup () {
-		parent.size(720, 720);
+		// NON USARE IL COMANDO SIZE!!
+		// parent.size(720, 720);
 		parent.noCursor();
 	}
 	
 	public void draw() {
-		  colorMode(HSB, 360, 100, 100);
-		  rectMode(CENTER); 
+		  parent.colorMode(PApplet.HSB, 360, 100, 100);
+		  parent.rectMode(PApplet.CENTER); 
 		  parent.noStroke();
 		  parent.background(parent.mouseY/2, 100, 100);
 
 		  parent.fill(360-parent.mouseY/2, 100, 100);
-		  parent.rect(360, 360, parent.mouseX+1, parent.mouseX+1);
+		  parent.rect(parent.width/2, parent.height/2, parent.mouseX+1, parent.mouseX+1);
 	}
 
 } 
