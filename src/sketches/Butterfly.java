d1a18d459b4d7b96864ef241979c67525cf39edb
matteotaramelli <illemaratoettam@gmail.com>
2013-05-03 19:21:34 +0200

    butterfly update
    errore fissato
    Sarebbe opportuno mantenere il valore di k (ciclo forLoop interno
    dataSender File Thread) minore di 4, altrimenti la grafica risulta
    sporca

--- src/sketches/Butterfly.java
+++ src/sketches/Butterfly.java
@@ -26,6 +26,7 @@ public class Butterfly extends EmptySketch {
 	ArrayList<Rcvr> recCollection= new ArrayList<Rcvr>();
 	float theta= 0;
 	float omega= (float) 3.1416 ;
+	float scale= (float) 0;
 	
 	int limit;
 	
@@ -35,8 +36,9 @@ public class Butterfly extends EmptySketch {
 	
 	public void draw() {
 		
+		scale+=0.1;
 
-		limit =  Math.min(recCollection.size(), 30);
+		limit =  Math.min(recCollection.size(), recCollection.size());
 		parent.background(0);
 		parent.smooth();
 		dataAL.clear();
@@ -91,6 +93,8 @@ public class Butterfly extends EmptySketch {
 		
 	private void execute () {	
 		
+		  float dim= PApplet.map(PApplet.sin(PApplet.radians(scale)),(float)-1,(float)1,(float)0.3,(float)0.5);
+		  float limitb=limit*(float)dim;
 		  float Rad= 3;
 
 		  for (int i=0; i<limit; i++) {
@@ -106,27 +110,29 @@ public class Butterfly extends EmptySketch {
 		    parent.noFill();
 		    parent.stroke(Math.abs(theRec.acc.z)*PApplet.sin(i/10)*100,parent.random(50,150));
 		    parent.strokeWeight((float)0.5*Math.abs(theRec.acc.y));
-		    parent.ellipse(cenX, cenY, References.height/5/limit*i*Rad, References.height/5/limit*i*Rad);
-		    parent.ellipse(cenX, cenY, References.height/10/limit*i*Rad, References.height/10/limit*i*Rad);
+		    parent.ellipse(cenX, cenY, (((References.height/5)/limitb))*i*Rad, (((References.height/5)/limitb)*i*Rad));
+		    parent.ellipse(cenX, cenY, (((References.height/10)/limitb))*i*Rad, (((References.height/10)/limitb)*i*Rad));
 		  }
+
 
 
 		  for (int i=0; i<limit; i++) {
 		    Rcvr theRec= (Rcvr) recCollection.get(i);
-
-		    float rotX= PApplet.cos(PApplet.radians(theRec.county))*(References.height/5/limit*i*Rad);
-		    float rotY= PApplet.sin(PApplet.radians(theRec.county))*(References.height/5/limit*i*Rad);
 		    
-		    float rotaX= PApplet.cos(PApplet.radians(theRec.county))*(References.height/10/limit*i*Rad);
-		    float rotaY= PApplet.sin(PApplet.radians(theRec.county))*(References.height/10/limit*i*Rad);
+		    float rotX= PApplet.cos(PApplet.radians(theRec.county))*(((References.height/5)/limitb)*i*Rad);
+		    float rotY= PApplet.sin(PApplet.radians(theRec.county))*(((References.height/5)/limitb)*i*Rad);
+		    
+		    float rotaX= PApplet.cos(PApplet.radians(theRec.county))*(((References.height/10)/limitb)*i*Rad);
+		    float rotaY= PApplet.sin(PApplet.radians(theRec.county))*(((References.height/10)/limitb)*i*Rad);
 		    
 		    float cenX= References.width/2;
 		    float cenY= References.height/2+ PApplet.sin(PApplet.radians(theRec.countx))*50;
-		    float thick=PApplet.map(Math.abs(theRec.acc.z), 0, 2, 5, 30);
+		    float thick=PApplet.map(Math.abs(theRec.acc.z), 0, 2, 2, 10);
 		    parent.noStroke();
-		    parent.fill(20*i,1000/(i+1) , 300/(i+1), PApplet.map(Math.abs(theRec.acc.z), 0, 2, 50, 250));
+		    parent.fill(20*i,2000/(i+1) , 300/(i+1), PApplet.map(Math.abs(theRec.acc.z), 0, 2, 50, 250));
 		    parent.ellipse(cenX+rotX, cenY+rotY, thick*2, thick*2);
 		    parent.ellipse(cenX-rotX, cenY+rotY, thick*2, thick*2);
+		    parent.fill(20*i,2000/(i+1) , 300/(i+1), PApplet.map(Math.abs(theRec.acc.z), 0, 2, 50, 150));
 		    parent.ellipse(cenX+rotaX, cenY-rotaY, thick, thick);
 		    parent.ellipse(cenX-rotaX, cenY-rotaY, thick, thick);
 
@@ -135,38 +141,44 @@ public class Butterfly extends EmptySketch {
 		    for (int j=0; j<limit; j++) {
 		      Rcvr othRec= (Rcvr) recCollection.get(j);
 
-		      float rotOX= PApplet.cos(PApplet.radians(othRec.county))*(References.height/5/limit*j*2);
-		      float rotOY= PApplet.sin(PApplet.radians(othRec.county))*(References.height/5/limit*j*2);
-		      float rotRX= (float)(PApplet.cos(PApplet.radians(othRec.county))*(References.height/5/limit*j*2.05));
-		      float rotRY= (float)(PApplet.sin(PApplet.radians(othRec.county))*(References.height/5/limit*j*2.05));
+		      float rotOX= PApplet.cos(PApplet.radians(othRec.county))*(References.height/5/limitb*j*2);
+		      float rotOY= PApplet.sin(PApplet.radians(othRec.county))*(References.height/5/limitb*j*2);
+		      float rotRX= (float)(PApplet.cos(PApplet.radians(othRec.county))*(References.height/5/limitb*j*2.05));
+		      float rotRY= (float)(PApplet.sin(PApplet.radians(othRec.county))*(References.height/5/limitb*j*2.05));
 
-		      float rotaOX= PApplet.cos(PApplet.radians(othRec.county))*(References.height/10/limit*j*2);
-		      float rotaOY= PApplet.sin(PApplet.radians(othRec.county))*(References.height/10/limit*j*2);
-		      float rotaRX= (float)(PApplet.cos(PApplet.radians(othRec.county))*(References.height/10/limit*j*2.05));
-		      float rotaRY= (float)(PApplet.sin(PApplet.radians(othRec.county))*(References.height/10/limit*j*2.05));
+		      //float rotaOX= PApplet.cos(PApplet.radians(othRec.county))*(References.height/10/limitb*j*2);
+		      //float rotaOY= PApplet.sin(PApplet.radians(othRec.county))*(References.height/10/limitb*j*2);
+		      //float rotaRX= (float)(PApplet.cos(PApplet.radians(othRec.county))*(References.height/10/limitb*j*2.05));
+		      //float rotaRY= (float)(PApplet.sin(PApplet.radians(othRec.county))*(References.height/10/limitb*j*2.05));
 
 		      
 		      
 		      
 		      
-		      if (theRec.acc.y> othRec.acc.y-0.01 && theRec.acc.y < othRec.acc.y+0.01) {
+		      if (theRec.acc.y> othRec.acc.y-0.01 && theRec.acc.y < othRec.acc.y+0.01 ) {
 
 		        parent.strokeWeight(1);
-		        parent.stroke(Math.abs(othRec.acc.z)*50, 20*i, 20*j, Math.abs(theRec.acc.z)*50);
+		        parent.stroke(Math.abs(othRec.acc.z)*50, 10*i, 10*j, Math.abs(theRec.acc.z)*50);
 		        
 		        parent.line(cenX+rotX, cenY+rotY, cenX+rotOX,cenY+rotOY);
 		        parent.line(cenX-rotX, cenY+rotY, cenX-rotOX, cenY+rotOY);
 		        
-		        parent.line(cenX+rotaX, cenY+rotaY, cenX+rotaOX,cenY+rotaOY);
-		        parent.line(cenX-rotaX, cenY+rotaY, cenX-rotaOX, cenY+rotaOY);
+		        //parent.stroke(Math.abs(othRec.acc.z)*50, 20*i, 20*j, 30);
+		        
+		        //parent.line(cenX+rotaX, cenY+rotaY, cenX+rotaOX,cenY+rotaOY);
+		        //parent.line(cenX-rotaX, cenY+rotaY, cenX-rotaOX, cenY+rotaOY);
 		        
 		        parent.strokeWeight(3);
+		        
+		        parent.stroke(Math.abs(othRec.acc.z)*50, 10*i, 10*j, Math.abs(theRec.acc.z)*50);
 		        
 		        parent.line(cenX+rotRX,cenY+rotRY, cenX+rotOX, cenY+rotOY);
 		        parent.line(cenX-rotRX,cenY+rotRY, cenX-rotOX, cenY+rotOY);
 		        
-		        parent.line(cenX+rotaRX,cenY+rotaRY, cenX+rotaOX, cenY+rotaOY);
-		        parent.line(cenX-rotaRX,cenY+rotaRY, cenX-rotaOX, cenY+rotaOY);
+		        //parent.stroke(Math.abs(othRec.acc.z)*50, 20*i, 20*j, 50);
+		        
+		        //parent.line(cenX+rotaRX,cenY+rotaRY, cenX+rotaOX, cenY+rotaOY);
+		        //parent.line(cenX-rotaRX,cenY+rotaRY, cenX-rotaOX, cenY+rotaOY);
 		      }
 		    }
 		  }
